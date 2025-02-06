b99afc4d00de9590ec5b1c93f7a6722281b4ed02
HarshSindhi1109 <harshsindhi085@gmail.com>
2025-02-06 19:53:08 +0530

    Modifying Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,48 @@
 package org.example;
 
 
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+
 public class Main {
     public static void main(String[] args) {
+        Queue<Integer> queue = new ArrayBlockingQueue<>(7);
+        System.out.println(queue.poll());
+        try {
+//            queue.offer(1);
+//            queue.offer(2);
+//            queue.offer(3);
+//            queue.offer(4);
+//            queue.offer(5);
+//            queue.offer(6);
+//            queue.offer(7);
+//            queue.remove();
+//            queue.offer(0);
+            queue.offer(1);
+            queue.offer(2);
+            queue.offer(3);
+            queue.offer(4);
+            queue.offer(5);
+            queue.offer(6);
+            queue.offer(7);
+            queue.remove();
+            queue.offer(0);
+            System.out.println(queue.offer(1));
 
+            /* *
+               * Major difference between .add() and .offer() is,
+               * in .add(), if the queue is full, it will throw exception.
+               * while in .offer(), if the queue is full, it will give message in form of false
+               * and if not, it will give true.
+               *
+               * Similar to that,
+               * .remove() will throw exception if queue is empty.
+               * while .poll() will give message if queue is empty.
+            */
+        } catch (Exception e) {
+            System.out.println("Error: "+ e.getMessage());
+        }
+        System.out.println(queue.peek());
+        System.out.println(queue);
     }
 }
