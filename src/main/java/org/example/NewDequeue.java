02ff777571848035fdca2f5d701ff2e0f76c7e38
HarshSindhi1109 <harshsindhi085@gmail.com>
2025-02-07 17:48:49 +0530

    Initial Commit

--- src/main/java/org/example/NewDequeue.java
+++ src/main/java/org/example/NewDequeue.java
@@ -1,4 +1,20 @@
 package org.example;
 
+import java.util.Deque;
+import java.util.concurrent.LinkedBlockingDeque;
+
 public class NewDequeue {
+    public void dequeue() {
+        Deque<Integer> deque = new LinkedBlockingDeque<>();
+
+        deque.offer(101);
+        deque.offer(102);
+        deque.offer(103);
+        deque.offer(104);
+        deque.offer(105);
+        deque.addFirst(100);
+        deque.removeLast();
+
+        System.out.println(deque);
+    }
 }
